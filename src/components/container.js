7d576ebb7c5308642fbe507d8a9d02b5fb664e41
Chester ONeill <coneillcodes@gmail.com>
2022-01-23 17:06:22 -0500

    just add className

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,3 @@
 import React from 'react';
 import containerStyles from './github.module.scss';
-export default ({ children }) => (
-  <div className={containerStyles.container}>{children}</div>
-);
+export default ({ children }) => <div className="container">{children}</div>;
